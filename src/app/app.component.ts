0f31074c7a8dcc1d53134c7031c30281ff53bce1
jonathas fonseca <jonathas09@gmail.com>
2020-08-17 15:18:41 -0300

    criada regras de negocios do form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Client } from './models/Client';
+import { ClientsService } from './service/clients.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'cedro';
+  client = {} as Client;
+  clients: Client[];
+
+  constructor(private clientService: ClientsService) {}
+
+  ngOnInit() { this.getClients(); }
+
+  postClient(form: NgForm) {
+    if(this.client.id !== undefined) {
+      this.clientService.updateClient(this.client).subscribe(() => {
+        this.cleanForm(form);
+      })
+    } else {
+      this.clientService.postClient(this.client).subscribe(() => {
+        this.cleanForm(form);
+      })
+    }
+  }
+
+  getClients() {
+    this.clientService.getClients().subscribe((clients: Client[]) => {
+      this.clients = clients;
+    })
+  }
+
+  cleanForm(form: NgForm) {
+    this.getClients();
+    form.resetForm();
+    this.client = {} as Client;
+  }
 }
