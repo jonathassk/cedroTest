fa3f90d773ef70b560d6e3f929fd368db58581c7
jonathas fonseca <jonathas09@gmail.com>
2020-08-18 03:30:47 -0300

    design arrumado e correçao da interface de modelo

--- src/app/models/Client.ts
+++ src/app/models/Client.ts
@@ -2,9 +2,9 @@ export interface Client {
   id: string;
   name: string;
   cpf: string;
-  cep: string;
-  logradouro: string;
-  bairro: string;
-  localidade: string;
-  uf: string;
+  zip: string;
+  address: string;
+  neighborhood: string;
+  city: string;
+  state: string;
 }
